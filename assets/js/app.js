c0dfd86e0ec0059e60431754f765289d2d1d9f98
Tania Falcon <taniafalconlopez1@gmail.com>
2024-11-05 17:47:57 -0600

    revicion de codigo 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,31 @@
 const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const usersEndpoint = ${baseEndpoint}/users;
+const $n = document.querySelector('.name'); //Este está mal escrito porque para llamar a una clase, se debe poner .name
+const $b = document.querySelector('.blog');//Este está mal porque no es un ID, sino una clase, por lo tanto sería .blog
+const $l = document.querySelector('.location'); //Este no apunta a nada porque no existe una clase="location"
 
-function displayUser(username) {
+async function displayUser(username) { /* Faltaba agregar async */
+  try{ //Se agregó un Try y un Catch
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+  const response = await fetch(${usersEndpoint}/${username}); /* response no está siendo llamada en ninguna parte. Posiblemente querría decir usersEndpoint */
+  const data = await response.json()
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = ${data.name}; //Se cambiaron las comillas para un correcto funcionamiento
+  $b.textContent = ${data.blog};
+  $l.textContent = ${data.location};
 }
+} catch (error) {
+  console.log(error);
+}
+
+
+
+getAllProducts();
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent = Algo salió mal: ${err}
 }
 
 displayUser('stolinski').catch(handleError);
